f923914e41d6cdfdf14b6e00252044fb37dab25f
sudiptoshahin <sudipto.shine@gmail.com>
2024-08-10 00:40:18 +0600

    singleton implement in logger

--- singleton/logger.py
+++ singleton/logger.py
@@ -1,76 +1,57 @@
 import os
-
-"""
-# loggerV1
-"""
-
-# def write_log(level, msg):
-#     with open(f"""{os.getcwd()}
-#     +/log/test_log1.log""", "a") as log_file:
-#     log_file.write("[{0}] {1}\n".format(level, msg))
-
-# def critical(msg)    :
-#     write_log('CRITICAL', msg)
-
-# def error(msg):
-#     write_log('ERROR', msg)
-
-# def warn(msg):
-#     write_log('WARN', msg)
-
-# def info(msg):
-#     write_log('INFO', msg)
-
-# def debug(msg):
-#     write_log('DEBUG', msg)
-    
-        
-"""
-# loggerV2
-"""
-
+from pathlib import Path
 class Logger(object):
-    """
-    A file based message logger with the following 
-    properties
+    _instance = None
+    _file_name = None
 
-    Attributes:
-        file_name: a string representing the full path
-        of the log file to which this logger will write
-        its messages
-    """
-    def __init__(self, file_name):
-        """
-        Returns a Logger object whose file_name is *file_name*
-        """
-        self.file_name = file_name
+    def __new__(cls) -> None:
+        if cls._instance is None:
+            cls._instance = super(Logger, cls).__new__(cls)
+        return cls._instance
+    
+    def _initialize(self) -> None:
+        if self._file_name is None:
+            log_dir: str = os.path.join(os.getcwd(), "log")
+            os.makedirs(log_dir, exist_ok=True)
+            temp_log_file: str = os.path.join(log_dir, "test_log.log")
+            self._file_name = Path(temp_log_file)
+            if not self._file_name.exists():
+                with open(self._file_name, "x", encoding="utf-8") as file:
+                    print(file)
 
-    def _write_log(self, level, msg):
-        """
-        writes a message to the file+Name for a specific
-        Logger instance
-        """
-        # try:
-        #     with open(self.file_name, "a+") as log_file:
-        #         log_file.write("[{0}] {1}".format(level, msg))
-        # except:
-        #     print('file not created')
-        with open(self.file_name, "a") as log_file:
-            log_file.write("\n[{0}] {1}".format(level, msg))
 
-        
-        
-    def critical(self, msg):
-        self._write_log('CRITICAL', msg)
-    
-    def error(self, msg):
-        self._write_log('ERROR', msg)
-    
-    def warn(self, msg):
-        self._write_log('WARN', msg)
-    
-    def info(self, msg):
+    def __init__(self) -> None:
+        self._initialize()
+
+    def _write_log(self, level: str, msg: str)-> None:
+        print(f'path: {self._file_name}')
+        with open(self._file_name, "a", encoding="utf-8") as log_file:
+            log_file.write(f"\n[{level}] {msg}")
+
+    def critical(self, msg:str)-> None:
+        """cricitical"""
+        self._write_log("CRITICAL", msg)
+
+    def error(self, msg: str)-> None:
+        """error"""
+        self._write_log("ERROR", msg)
+
+    def warn(self, msg: str)-> None:
+        """warn"""
+        self._write_log("WARN", msg)
+    def debug(self, msg: str)-> None:
+        """debug"""
+        self._write_log("DEBUG", msg)
+    def info(self, msg: str)-> None:
+        """info"""
         self._write_log("INFO", msg)
 
-    def debug(self, msg):
-        self._write_log('DEBUG', msg)
+if __name__ == "__main__":
+    l1 = Logger()
+    l2 = Logger()
+
+    l1.error("Hello world")
+    l2.critical("This is very critical situation.")
+
+    print(f"l1: {l1}")
+    print(f"l2: {l2}")
