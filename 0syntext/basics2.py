c0c9a915c328a2dc9c9941e87d01d429b7d9af13
sudiptoshahin <sudipto.shine@gmail.com>
2024-08-10 00:58:40 +0600

    basic | gitignore

--- 0syntext/basics2.py
+++ 0syntext/basics2.py
@@ -1,8 +1,9 @@
-
+from typing import List
 
 def find_cube_root(x: int) -> int:
     ans: int = 0
     while ans**3 < abs(x):
+        print(f"Value of the decrementing function abs(x) - ans**3 is {abs(x) - ans**3}")
         ans = ans + 1
     if ans**3 != abs(x):
         print(f"{x} is not a perfect cube.")
@@ -11,8 +12,17 @@ def find_cube_root(x: int) -> int:
             ans = -ans
         print(f"Cube root of {x} is {ans}")
 
+def rootPwr(num: int) -> List[int]:
+    root: float = num ** 0.5
+    pwr: int
+    
+
 
 if __name__ == '__main__':
     # find the cube root of a perfect cube
     x: int = int(input("Enter a number: "))
-    print(find_cube_root(x))
+    # print(find_cube_root(x))
+    i = 0
+    while i < x:
+        i = i + 1
+    print(i)
